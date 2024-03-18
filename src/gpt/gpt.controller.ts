249db22a69ee998434d89d564c8e84d2082ab799
UrielBm <urielbenitez96@hotmail.com>
2024-03-18 16:22:47 -0600

    base of consDiscusser endpoint

--- src/gpt/gpt.controller.ts
+++ src/gpt/gpt.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
 import { GptService } from './gpt.service';
-import { orthographyDto } from './dtos';
+import { ProsConsDiscusserDto, orthographyDto, translateDto } from './dtos';
+import { Response } from 'express';
 
 @Controller('gpt')
 export class GptController {
@@ -8,5 +9,20 @@ export class GptController {
   @Post('orthography-ckeck')
   orthographyCheck(@Body() body:  orthographyDto){
     return this.gptService.orthographyCheck(body)
+  }
+  @Post("pros-cons-disscusser")
+  async prosConsDiscusser(@Body() data: ProsConsDiscusserDto,@Res() res: Response){
+    const stream = await this.gptService.prosConsDicusser(data);
+    res.setHeader('Content-Type', 'application/json')
+    res.status(HttpStatus.OK)
+    for await ( const chunk of stream){
+      const piece = chunk.choices[0].delta.content || '';
+      res.write(piece)
+    }
+    res.end();
+  }
+  @Post("translate")
+  async translateMethod(@Body() dtoTranslate: translateDto ){
+    return this.gptService.translateText(dtoTranslate)
   }
 }
